a8980c038c2c9c23f6d98a9d473ebae3f8546c69
James Curtis <james.curtis@opusvl.com>
2017-07-28 09:33:10 +0100

    [REF] Refactor licence

--- partner_supplier_invoice_smartbutton/__openerp__.py
+++ partner_supplier_invoice_smartbutton/__openerp__.py
@@ -1,27 +1,28 @@
- # -*- coding: utf-8 -*-
-##############################################################################
+# -*- coding: utf-8 -*-
+
+# //////////////////////////////////////////////////////////////////////
 #
-#    Odoo, Open Source Management Solution, extensions for AMSBIO
-#    Copyright (C) 2013 OpusVL (<http://opusvl.com>).
+# Supplier Invoice Smart Button
+# Copyright (C) 2017 OpusVL (<http://opusvl.com/>)
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+# //////////////////////////////////////////////////////////////////////
 
 
 {
-    'name': 'AMSBio Partner Supplier Invoice Smart Button',
+    'name': 'Partner Supplier Invoice Smart Button',
     'version': '0.1',
     'category': 'Accounting',
     'description': """
@@ -41,4 +42,5 @@
     'installable': True,
     'auto_install': False,
 }
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
--- partner_supplier_invoice_smartbutton/models/account_partner.py
+++ partner_supplier_invoice_smartbutton/models/account_partner.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
-from openerp.osv import fields, osv
+
+# //////////////////////////////////////////////////////////////////////
+#
+# Supplier Invoice Smart Button
+# Copyright (C) 2017 OpusVL (<http://opusvl.com/>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# //////////////////////////////////////////////////////////////////////
 
 # Generic module to add a 'Supplier Invoice Total' smart button to the partner view
 # Method takes a kwarg 'price_field' that defaults to the base Odoo field which can be used for custom cases
+
+from openerp.osv import fields, osv
 
 
 class res_partner(osv.osv):
